e565b6aa6e8170d9e7638eaa88007eecdd9c5aca
y00ss <youssef.dev@pm.me>
2024-06-13 23:41:32 +0200

    application app

--- src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplication.java
+++ src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplication.java
@@ -1,4 +1,25 @@
 package io.quarkus.workshop.superheroes.villain;
 
-public class VillainApplication {
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
+
+@ApplicationPath("/")
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Villain API",
+        description = "This API allows CRUD operations on a villain",
+        version = "1.0",
+        contact = @Contact(name = "Villain", url = "https://github.com/y00ss")
+    ),
+    servers = { @Server(url = "http://localhost:8084") },
+    externalDocs = @ExternalDocumentation(url = "https://github.com/y00ss", description = "Enjoying quarkus")
+)
+public class VillainApplication  extends Application {
+    // empty on propose
 }
--- src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplicationLifeCycle.java
+++ src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplicationLifeCycle.java
@@ -1,4 +1,28 @@
 package io.quarkus.workshop.superheroes.villain;
 
+import io.quarkus.runtime.ShutdownEvent;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.jboss.logging.Logger;
+
+
+@ApplicationScoped
 public class VillainApplicationLifeCycle {
+
+    private static final Logger LOGGER = Logger.getLogger(VillainApplicationLifeCycle.class);
+
+    void onStart(@Observes ShutdownEvent ev) {
+        LOGGER.info(" __     ___ _ _       _             _    ____ ___ ");
+        LOGGER.info(" \\ \\   / (_) | | __ _(_)_ __       / \\  |  _ \\_ _|");
+        LOGGER.info("  \\ \\ / /| | | |/ _` | | '_ \\     / _ \\ | |_) | | ");
+        LOGGER.info("   \\ V / | | | | (_| | | | | |   / ___ \\|  __/| | ");
+        LOGGER.info("    \\_/  |_|_|_|\\__,_|_|_| |_|  /_/   \\_\\_|  |___|");
+
+        LOGGER.info("Villain service is runninggk DKDKkaSN");
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application VILLAIN is stopping...");
+    }
 }
