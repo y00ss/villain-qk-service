254bfb682e3476584bcdfc3f53e90a9175b6664f
y00ss <youssef.dev@pm.me>
2024-06-14 23:22:38 +0200

    fix log and observe by startEvent

--- src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplicationLifeCycle.java
+++ src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplicationLifeCycle.java
@@ -2,6 +2,8 @@ package io.quarkus.workshop.superheroes.villain;
 
 import io.quarkus.runtime.ShutdownEvent;
 
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ConfigUtils;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import org.jboss.logging.Logger;
@@ -12,14 +14,14 @@ public class VillainApplicationLifeCycle {
 
     private static final Logger LOGGER = Logger.getLogger(VillainApplicationLifeCycle.class);
 
-    void onStart(@Observes ShutdownEvent ev) {
+    void onStart(@Observes StartupEvent ev) {
         LOGGER.info(" __     ___ _ _       _             _    ____ ___ ");
         LOGGER.info(" \\ \\   / (_) | | __ _(_)_ __       / \\  |  _ \\_ _|");
         LOGGER.info("  \\ \\ / /| | | |/ _` | | '_ \\     / _ \\ | |_) | | ");
         LOGGER.info("   \\ V / | | | | (_| | | | | |   / ___ \\|  __/| | ");
         LOGGER.info("    \\_/  |_|_|_|\\__,_|_|_| |_|  /_/   \\_\\_|  |___|");
 
-        LOGGER.info("Villain service is runninggk DKDKkaSN");
+        LOGGER.info("Villain service is running  with profile : " + ConfigUtils.getProfiles());
     }
 
     void onStop(@Observes ShutdownEvent ev) {
